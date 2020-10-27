cf9d621d21364a0f1f9694c358bb08f3f9346f75
纪章鹏 <1208235155@qq.com>
2020-10-27 16:49:57 +0800

    数据挖掘学习_猜数字游戏
    while True:方法，continue：跳过本次循环，进行下一轮循环。break：退出循环。
    添加：数字判断分支，结束游戏按钮。

--- guess_number.py
+++ guess_number.py
@@ -6,15 +6,23 @@ def guess(num):
     count = 0
     while True:
         count += 1
-        number = int(input('请输入你心中的数字：'))
-        if num == number:
-            print('恭喜你，你猜了{0}次终于猜对了数字{1}'.format(count,num))
+        number = input('请输入你心中的数字：')
+        if number == 'quit':
+            print('结束游戏，你输了！')
             break
-        elif number > num:
+        elif not number.isdigit() or int(number) not in range(1,100):
+            print('请输入1-99之间的整数！')
+            continue
+        elif int(number) == num:
+            print('恭喜你，你猜了{0}次终于猜对了数字{1}!'.format(count,num))
+            break
+        elif int(number) > num:
             print('你猜大了')
-        elif number < num:
+        elif int(number) < num:
             print('你猜小了')
 
 #生成一个1-100之间的随机数
-num = random.randint(1,100)
+num = random.randint(1,99)
+print('猜数字游戏')
+print('请猜一个1到99之间的随机数字，输入‘quit’退出游戏')
 guess(num)
